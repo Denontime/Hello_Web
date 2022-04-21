315cbf82c35715f17680a393c65fd0750f41a334
Mar ping <51920644+Denontime@users.noreply.github.com>
2022-04-21 20:41:55 +0800

    合并

---
+++ Demo/Test/js/Tawer.js
@@ -0,0 +1,11 @@
+/*
+ * @Descripttion: Null
+ * @version: 1.0
+ * @Author: Mar Ping
+ * @Date: 2020-12-31 09:17:43
+ * @LastEditors: Mar Ping
+ * @LastEditTime: 2020-12-31 23:19:20
+ */
+for (var i = 10; i >= -10; i--) {
+    document.write("<hr height=20px width=" + 10 * Math.abs(i) + "%/>")
+}
